856fb2763235be8aab7bdc74cd2fc76ff91ca609
Oscar Felix <ofelix60@gmail.com>
2023-03-11 10:04:57 -0800

    Sad servers #1.

---
+++ sad_servers/01.saint_john.sh
@@ -0,0 +1,16 @@
+# Scenario: "Saint John": what is writing to this log file?
+
+# Level: Easy
+
+# Description: A developer created a testing program that is continuously writing to a log file /var/log/bad.log and filling up disk. You can check for example with tail -f /var/log/bad.log.
+# This program is no longer needed. Find it and terminate it.
+
+# Test: The log file hasn't changed in the last 6 seconds: find /var/log/bad.log -mmin -0.1 (You don't need to know the details of this command).
+
+# Time to Solve: 10 minutes.
+
+# OS: Ubuntu 22.04 LTS
+
+sudo rm /var/log/bad.log
+cd /var/log/
+ls
